da09e54aa4bdc65a5d0eb3b91c1f5e3e19e6f6a8
Michael Lee <lee.cs.michael@gmail.com>
2018-09-30 18:10:00 -0700

    Extend existing API to support POST to database

--- server/middlewares/addDevMiddlewares.js
+++ server/middlewares/addDevMiddlewares.js
@@ -2,6 +2,8 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const bodyParser = require('body-parser');
+const Message = require('../../database/index');
 
 function createWebpackMiddleware(compiler, publicPath) {
   return webpackDevMiddleware(compiler, {
@@ -21,6 +23,7 @@ module.exports = function addDevMiddlewares(app, webpackConfig) {
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
+  app.use(bodyParser.json());
 
   // Since webpackDevMiddleware uses memory-fs internally to store build
   // artifacts, we use it instead
@@ -32,6 +35,18 @@ module.exports = function addDevMiddlewares(app, webpackConfig) {
         res.sendStatus(404);
       } else {
         res.send(file.toString());
+      }
+    });
+  });
+
+  app.post('/messages', (req, res) => {
+    const message = new Message(req.body);
+    message.save(err => {
+      if (err) {
+        throw err;
+      } else {
+        console.log('message saved successfully...');
+        res.sendStatus(201);
       }
     });
   });
