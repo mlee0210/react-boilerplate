8309bca074c499aa359bff9271731d03e22376f2
Michael Lee <lee.cs.michael@gmail.com>
2018-09-30 18:35:20 -0700

    Create UI to input messages

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,50 @@
  */
 
 import React from 'react';
+import axios from 'axios';
 import { FormattedMessage } from 'react-intl';
+import Form from './Form';
+import Input from './Input';
 import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+    this.onChangeMessage = this.onChangeMessage.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+  onSubmitForm(e) {
+    e.preventDefault();
+    const message = { message: this.state.message };
+    axios
+      .post('/messages', message)
+      .then(results => {
+        console.log('Message saved', results);
+        this.setState({ message: '' });
+      })
+      .catch(err => console.log('Message not saved', err));
+  }
+  onChangeMessage(e) {
+    this.setState({ message: e.target.value });
+  }
+
   render() {
     return (
       <h1>
         <FormattedMessage {...messages.header} />
+        <Form onSubmit={this.onSubmitForm}>
+          <Input
+            id="message"
+            type="text"
+            placeholder="message"
+            value={this.state.message}
+            onChange={this.onChangeMessage}
+          />
+        </Form>
       </h1>
     );
   }
